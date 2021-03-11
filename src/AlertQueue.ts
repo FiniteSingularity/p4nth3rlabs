59617757e2c08d0573f99204111b2a8ae4fa8c6d
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-03-11 17:03:54 +0000

    :sparkles: Change alert time for Giveaway announcement

--- src/AlertQueue.ts
+++ src/AlertQueue.ts
@@ -1,5 +1,6 @@
-import { Dispatch, useContext, useEffect, useState } from 'react';
-import AppContext, { AlertCompleteAction, AlertQueueEvent } from './AppContext';
+import { Dispatch, useContext, useEffect, useState } from "react";
+import AppContext, { AlertCompleteAction, AlertQueueEvent } from "./AppContext";
+import { MainframeEvent } from "p4nth3rb0t-types";
 
 export function useAlertQueue(dispatch: Dispatch<any>): AlertQueueEvent | null {
   const ctx = useContext(AppContext);
@@ -9,7 +10,14 @@ export function useAlertQueue(dispatch: Dispatch<any>): AlertQueueEvent | null {
     if (ctx.state.alerts.length === 0) setCurrentAlert(null);
     else if (currentAlert?.id !== ctx.state.alerts[0].id) {
       setCurrentAlert(ctx.state.alerts[0]);
-      setTimeout(() => dispatch({ event: 'alert_complete' } as AlertCompleteAction), 5000);
+
+      const alertDisplayTime =
+        ctx.state.alerts[0].event === MainframeEvent.announceGiveaway ? 19000 : 5000;
+
+      setTimeout(
+        () => dispatch({ event: "alert_complete" } as AlertCompleteAction),
+        alertDisplayTime,
+      );
     }
   }, [ctx, dispatch, currentAlert]);
 
