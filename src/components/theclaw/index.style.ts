559e137d27343a9c1b6f0839154d3ce21d8c1108
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-10-27 15:24:13 +0100

    :sparkles: Add shadow to claw shout out images

--- src/components/theclaw/index.style.ts
+++ src/components/theclaw/index.style.ts
@@ -16,6 +16,7 @@ const ImageContainer = styled(motion.div)`
   position: relative;
   background-color: var(--black);
   transform: rotate3d(1, 0, 0, 90deg);
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
 `;
 
 const Image = styled.img`
