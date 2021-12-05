dd1a01e3b1271d8aa7cda5eb3ffc1c1cd7ea708f
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-12-05 19:36:46 +0000

    :lipstick: Breaking news ticker style updates

--- src/components/SpecialAnnouncement/index.style.ts
+++ src/components/SpecialAnnouncement/index.style.ts
@@ -23,11 +23,12 @@ const TopBar = styled(motion.div)`
   padding: 0.5rem 1rem;
   display: block;
   overflow: hidden;
+  box-shadow: 0rem 0.6rem 1rem -0.4rem var(--black);
 `;
 
 const TopBarText = styled.p`
   font-weight: var(--font-weight-bold);
-  font-size: 1rem;
+  font-size: 1.6rem;
   color: var(--white);
   text-transform: uppercase;
   white-space: nowrap;
