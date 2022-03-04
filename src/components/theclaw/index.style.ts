eb447a2999c9291445e6b0f0354fb9aefcbd23c5
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-03-04 10:54:51 +0000

    Add circles to claw overlay

--- src/components/theclaw/index.style.ts
+++ src/components/theclaw/index.style.ts
@@ -2,52 +2,48 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 const Container = styled.div`
-  max-width: 760px;
-  margin: auto;
-  align-self: center;
-  justify-self: center;
-  display: grid;
-  margin-top: 10rem;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  position: relative;
+  height: 1080px;
+  width: 1920px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
 `;
 
-const ImageContainer = styled(motion.div)`
-  position: relative;
-  background-color: var(--black);
-  transform: rotate3d(1, 0, 0, 90deg);
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
-`;
+interface ImageContainerProps {
+  size: number;
+  coords: {
+    x: number;
+    y: number;
+  };
+}
 
-const Image = styled.img`
-  width: 100%;
-  height: 100%;
-`;
-
-const Name = styled.p`
+const Image = styled(motion.img)<ImageContainerProps>`
   position: absolute;
-  bottom: 0;
   background-color: var(--black);
-  color: var(--yellow);
-  font-size: 1rem;
+  left: ${(props) => props.coords.x - props.size}px;
+  bottom: ${(props) => props.coords.y - props.size}px;
+  height: ${(props) => props.size * 2}px;
+  width: ${(props) => props.size * 2}px;
+  border-radius: 50%;
+  border: 0.125rem solid var(--black);
   box-sizing: border-box;
-  padding: 0.5rem 1rem;
-  font-weight: var(--font-weight-bold);
-  z-index: 1;
+  transform: rotate(-60deg) scale(0);
 `;
 
 const MothContainer = styled(motion.div)`
-  width: 760px;
+  width: 500px;
   padding: 2rem;
   box-sizing: border-box;
   position: absolute;
   margin: auto;
   top: 50%;
-  transform: translateY(-50%) scale(1);
+  left: 50%;
+  transform: translate(-50%, -50%) scale(0);
 
   svg {
     filter: drop-shadow(0px 9px 8px rgb(0 0 0 / 0.6));
   }
 `;
 
-export { Container, Image, ImageContainer, Name, MothContainer };
+export { Container, Image, MothContainer };
--- src/components/theclaw/index.tsx
+++ src/components/theclaw/index.tsx
@@ -1,7 +1,82 @@
 import { Dispatch, useContext, useEffect } from "react";
 import AppContext, { RemoveClawShoutOutAction } from "../../AppContext";
-import { Container, Image, ImageContainer, Name, MothContainer } from "./index.style";
+import { Container, Image, MothContainer } from "./index.style";
 import MothAndBanner from "./MothAndBanner";
+
+type CoOrd = { x: number; y: number };
+
+function getCircles(outerRadius: number, targetCircles: number, centerX = 960, centerY = 540) {
+  let totNumCircles = 0;
+  let curRadius = outerRadius;
+  while (totNumCircles < targetCircles) {
+    totNumCircles = getNumCircles(outerRadius, curRadius);
+    curRadius -= 0.1;
+  }
+  const innerRadius = curRadius;
+  let curOuterRadius = outerRadius;
+  let circleLocations: CoOrd[] = [];
+  while (outerRadius > 0) {
+    const numCircles = ringCount(curOuterRadius, innerRadius);
+    if (numCircles === 0) {
+      break;
+    }
+    circleLocations = [
+      ...circleLocations,
+      ...getRing(curOuterRadius, numCircles, centerX, centerY),
+    ];
+    curOuterRadius -= innerRadius * 2;
+  }
+  return {
+    circleRadius: innerRadius,
+    locations: circleLocations,
+  };
+}
+
+function getRing(ringRadius: number, numCircles: number, centerX: number, centerY: number) {
+  const deltaTheta = (Math.PI * 2.0) / numCircles;
+  if (deltaTheta < 0) {
+    return [];
+  } else if (numCircles === 1) {
+    return [{ x: centerX, y: centerY }];
+  } else {
+    const locations = [];
+    for (let i = 0; deltaTheta * i < 2.0 * Math.PI; i++) {
+      const theta = deltaTheta * i;
+      const x = Math.cos(theta) * ringRadius + centerX;
+      const y = Math.sin(theta) * ringRadius + centerY;
+      locations.push({ x, y });
+    }
+    return locations;
+  }
+}
+
+function getNumCircles(outerRadius: number, innerRadius: number) {
+  let numCircles = 0;
+  while (outerRadius > 0) {
+    const newCircles = ringCount(outerRadius, innerRadius);
+    if (newCircles === 0) {
+      break;
+    }
+    numCircles += newCircles;
+    outerRadius -= innerRadius * 2;
+  }
+  return numCircles;
+}
+
+function ringCount(outerRingRadius: number, radius: number) {
+  if (radius > outerRingRadius) {
+    return 0;
+  } else if (radius > outerRingRadius / 2.0) {
+    return 1;
+  }
+  const numCircles = Math.floor((Math.PI * (outerRingRadius - radius)) / radius);
+  const x0 = outerRingRadius - radius;
+  const y0 = 0;
+  const x1 = (outerRingRadius - radius) * Math.cos((2 * Math.PI) / numCircles);
+  const y1 = (outerRingRadius - radius) * Math.sin((2 * Math.PI) / numCircles);
+  const dist = Math.pow(Math.pow(x0 - x1, 2) + Math.pow(y0 - y1, 2), 0.5);
+  return dist < 2 * radius ? numCircles - 1 : numCircles;
+}
 
 interface TheClawProps {
   dispatch: Dispatch<any>;
@@ -12,10 +87,7 @@ export default function TheClaw(props: TheClawProps) {
   const { clawShoutOut, clawShoutOutData } = state;
   const { dispatch } = props;
 
-  // temporarily remove whitep4nth3r from the members so we have an even grid :P
-  const teamMembersToShow = clawShoutOutData.filter(
-    (member) => member.displayName !== "whitep4nth3r",
-  );
+  const circles = getCircles(240, clawShoutOutData.length);
 
   useEffect(() => {
     if (clawShoutOut && clawShoutOutData.length > 0) {
@@ -36,38 +108,39 @@ export default function TheClaw(props: TheClawProps) {
           <audio autoPlay>
             <source src={process.env.REACT_APP_AUDIO_ALERT_SHOUTOUT} type="audio/mp3" />
           </audio>
-          {teamMembersToShow.map((member) => (
-            <ImageContainer
+          {clawShoutOutData.map((member, index) => (
+            <Image
               key={member.displayName}
+              size={circles.circleRadius}
+              coords={circles.locations[index]}
+              src={member.logoUrl}
+              alt={member.displayName}
               animate={{
                 transform: [
-                  "rotate3d(1, 1, 0, 90deg)",
-                  "rotate3d(0, 0, 0, 0deg)",
-                  "rotate3d(0, 0, 0, 0deg)",
-                  "rotate3d(-1, -1, 0, -90deg)",
+                  "rotate(-60deg) scale(0)",
+                  "rotate(0deg) scale(1)",
+                  "rotate(0deg) scale(1)",
+                  "rotate(60deg) scale(0)",
                 ],
               }}
               transition={{
-                duration: 5.5,
+                duration: 9,
                 ease: "easeInOut",
-                times: [0, 0.25, 0.75, 1],
+                times: [0, 0.1, 0.9, 1],
               }}
-            >
-              <Image src={member.logoUrl} alt={member.displayName} />
-              <Name>{member.displayName}</Name>
-            </ImageContainer>
+            />
           ))}
           <MothContainer
             animate={{
               transform: [
-                "translateY(-50%) scale(0)",
-                "translateY(-50%) scale(1)",
-                "translateY(-50%) scale(1)",
-                "translateY(-50%) scale(0)",
+                "translate(-50%, -50%) scale(0)",
+                "translate(-50%, -50%) scale(1)",
+                "translate(-50%, -50%) scale(1)",
+                "translate(-50%, -50%) scale(0)",
               ],
             }}
             transition={{
-              delay: 1.75,
+              delay: 2,
               duration: 6,
               ease: "easeInOut",
               times: [0, 0.25, 0.75, 1],
